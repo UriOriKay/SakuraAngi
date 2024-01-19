2d2b4c0bb69ab069478d2ddfa06e404650617321
UriOriKay <127621006+UriOriKay@users.noreply.github.com>
2024-01-19 09:53:06 +0100

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landingPage/landingPage.component';
+import { NavbarComponent } from './landingPage/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
